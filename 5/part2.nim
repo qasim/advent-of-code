3159c087f2c1f6c93bb6d53355888bf792f756f7
Qasim Iqbal <me@qas.im>
2019-12-05 02:42:01 -0500

    Add day 5 solutions (part 2)

---
+++ 5/part2.nim
@@ -0,0 +1,195 @@
+import sequtils, strutils, sugar
+
+type Instruction = tuple[
+  opcode: int,
+  mode1: int,
+  mode2: int,
+  mode3: int
+]
+
+proc instruction(integer: int): Instruction =
+  let integerString = intToStr(integer)
+
+  if integerString.len == 1 or integerString.len == 2:
+    return (
+      opcode: parseInt(integerString),
+      mode1: 0,
+      mode2: 0,
+      mode3: 0
+    )
+
+  elif integerString.len == 3:
+    return (
+      opcode: parseInt(integerString[1..^1]),
+      mode1: parseInt($integerString[0]),
+      mode2: 0,
+      mode3: 0
+    )
+
+  elif integerString.len == 4:
+    return (
+      opcode: parseInt(integerString[2..^1]),
+      mode1: parseInt($integerString[1]),
+      mode2: parseInt($integerString[0]),
+      mode3: 0
+    )
+
+  elif integerString.len == 5:
+    return (
+      opcode: parseInt(integerString[3..^1]),
+      mode1: parseInt($integerString[2]),
+      mode2: parseInt($integerString[1]),
+      mode3: parseInt($integerString[0])
+    )
+
+  echo "Something went wrong"
+  quit(1)
+
+proc executeIntcode(integers: var seq[int], input: int, position: int): int =
+  let instruction = instruction(integers[position])
+  var newPosition: int
+
+  case instruction.opcode:
+    of 1:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      integers[integers[position + 3]] = parameter1 + parameter2
+      newPosition = position + 4
+
+    of 2:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      integers[integers[position + 3]] = parameter1 * parameter2
+      newPosition = position + 4
+
+    of 3:
+      integers[integers[position + 1]] = input
+      newPosition = position + 2
+
+    of 4:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      echo parameter1
+      newPosition = position + 2
+
+    of 5:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      if parameter1 != 0:
+        newPosition = parameter2
+      else:
+        newPosition = position + 3
+
+    of 6:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      if parameter1 == 0:
+        newPosition = parameter2
+      else:
+        newPosition = position + 3
+
+    of 7:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      if parameter1 < parameter2:
+        integers[integers[position + 3]] = 1
+      else:
+        integers[integers[position + 3]] = 0
+
+      newPosition = position + 4
+
+    of 8:
+      var parameter1: int
+      if instruction.mode1 == 0:
+        parameter1 = integers[integers[position + 1]]
+      else:
+        parameter1 = integers[position + 1]
+
+      var parameter2: int
+      if instruction.mode2 == 0:
+        parameter2 = integers[integers[position + 2]]
+      else:
+        parameter2 = integers[position + 2]
+
+      if parameter1 == parameter2:
+        integers[integers[position + 3]] = 1
+      else:
+        integers[integers[position + 3]] = 0
+
+      newPosition = position + 4
+
+    of 99:
+      quit(0)
+
+    else:
+      echo "Something went wrong"
+      quit(1)
+
+  return executeIntcode(integers, input, newPosition)
+
+proc executeIntcode(integers: var seq[int], input: int): int =
+  return executeIntcode(integers, input, 0)
+
+proc main() =
+  let inputFile = open("input.txt")
+  let input = inputFile.readLine()
+  inputFile.close()
+
+  var integers = map(input.split(","), (x: string) => parseInt(x))
+  echo executeIntcode(integers, 5)
+
+main()
